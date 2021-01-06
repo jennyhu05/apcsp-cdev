e7a5fecdc09bd6a1aee933f306431904da97b363
Jenny Hu <huj1738@my.sduhsd.net>
2021-01-06 21:47:51 +0000

    variables.c

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+  a = 4;
+  c = 6;
+  printf("a = %d and b = %d\n", a, c);
+}
