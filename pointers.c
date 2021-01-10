95e7cb85fcb9c348b59e89ca212c8dd1fc6064fa
Jenny Hu <huj1738@my.sduhsd.net>
2021-01-10 02:12:56 +0000

    pointers

---
+++ pointers.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  int a;
+  int* ptrtoa;
+
+  ptrtoa = &a;
+
+  a = 5;
+  printf("The value of a is %d\n", a);
+
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+
+  printf("The value of ptrtoa is %p\n", (void*)ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %p\n", (void*)&a);
+
+  //activity
+  float d = 10;
+  float e = 15;
+
+  float* ptrtod = &d;
+  float* ptrtoe = &e;
+
+  printf("The value of d is %f\n", d);
+  printf("The address of d is %p\n", (void*)&d);
+
+  printf("The value of e is %f\n", e);
+  printf("The address of e is %p\n", (void*)&e);
+
+  float f = *ptrtod;
+  float g = *ptrtoe;
+
+  *ptrtod = 20;
+  *ptrtoe = 30;
+
+  printf("The new value of d is %f\n", d);
+  printf("The new value of e is %f\n", e);
+}
